313e56f1d7096fe07a27f16285ed9f236b9867cf
kingsportuy-bit <kingsportuy@gmail.com>
2025-10-24 12:29:21 -0300

    Actualizar animación de fondo: 300 estrellas, rotación de 120s, punto de rotación fuera de pantalla y corregir visualización de destellos

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Todos los derechos reservados | © 2025 <a href="https://codexa.uy" className="footer-link">Code<span className="codexa-x">x</span>a.uy</a>
+    </div>
+  );
+};
+
+export default Footer;
